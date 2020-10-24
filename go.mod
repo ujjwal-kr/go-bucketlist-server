c02a0ae45c01a95b32211935945158f27f64b8ad
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-24 16:38:37 +0530

    created fiber app!

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ujjwal-kr/go-bucketlist-server
+
+go 1.15
+
+require github.com/gofiber/fiber/v2 v2.1.1
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world in Fiber Server!")
+	})
+
+	app.Listen(":8080")
+}

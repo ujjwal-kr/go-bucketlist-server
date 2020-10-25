49cf0adefd750ed27bc08aabea64308b17852480
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-25 18:38:40 +0530

    mongo-db basic connection

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ujjwal-kr/go-bucketlist-server
 
 go 1.15
 
-require github.com/gofiber/fiber/v2 v2.1.1
+require (
+	github.com/gofiber/fiber/v2 v2.1.1
+	go.mongodb.org/mongo-driver v1.4.2
+)
--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/ujjwal-kr/go-bucketlist-server/list"
 	"github.com/ujjwal-kr/go-bucketlist-server/task"
 	"github.com/ujjwal-kr/go-bucketlist-server/user"
 )
+
+// MongoInstance contains the Mongo client and database objects
+
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+var mg MongoInstance
+
+const dbName = "mybucketlist"
+const mongoURI = "mongodb://localhost:27017/" + dbName
+
+// Connect configures the MongoDB client and initializes the database connection.
+func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	db := client.Database(dbName)
+
+	if err != nil {
+		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+
+	return nil
+}
 
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Helloworld")

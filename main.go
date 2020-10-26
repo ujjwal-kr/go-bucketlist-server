adbf3a718944666f0cff2b785ff6f2bd6a379b6b
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-26 18:15:24 +0530

    minimised file sharing

--- main.go
+++ main.go
@@ -9,15 +9,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/ujjwal-kr/go-bucketlist-server/list"
-	"github.com/ujjwal-kr/go-bucketlist-server/task"
-	"github.com/ujjwal-kr/go-bucketlist-server/user"
 )
 
 // MongoInstance contains the Mongo client and database objects
 
 const dbName = "mybucketlist"
 const mongoURI = "mongodb://localhost:27017/" + dbName
+
+// MongoInstance contains the Mongo client and database objects
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+var mg MongoInstance
 
 // Connect configures the MongoDB client and initializes the database connection.
 func Connect() error {
@@ -27,9 +32,15 @@ func Connect() error {
 	defer cancel()
 
 	err = client.Connect(ctx)
+	db := client.Database(dbName)
 
 	if err != nil {
 		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
 	}
 
 	return nil
@@ -37,26 +48,6 @@ func Connect() error {
 
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Helloworld")
-}
-
-func setupRoutes(app *fiber.App) {
-
-	app.Get("/", welcome)
-
-	app.Get("/users", user.GetUsers)
-	app.Get("/users/:id", user.GetUser) // gets the lists of the user along with the user
-	app.Post("/auth/login", user.Login)
-	app.Post("/auth/register", user.Register)
-	app.Get("/users/:id/tasks", user.GetTasks)
-
-	app.Post("/lists", list.PostList)
-	app.Get("/lists/:id", list.GetList)
-	app.Patch("/lists/:id", list.UpdateList)
-	app.Delete("/lists/:id", list.DeleteList)
-
-	app.Post("/tasks", task.PostTask)
-	app.Delete("/tasks/:id", task.DeleteTask)
-
 }
 
 func main() {
@@ -68,7 +59,7 @@ func main() {
 
 	app := fiber.New()
 
-	setupRoutes(app)
+	app.Get("/", welcome)
 
 	app.Listen(":8080")
 }
